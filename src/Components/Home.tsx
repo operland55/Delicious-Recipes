d4b0b71f6328fbd9eacb3e7d90031fcbc692a92d
jungpa <operland55@naver.com>
2023-01-01 11:38:42 +0900

    ✨ Home Component

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,33 +1,21 @@
 import React from "react";
-import Header from "./Header";
-import MenuSide from "./MenuSide";
-import { useRecoilValue } from "recoil";
-import { MenuBtn } from "../atom";
+import HomeAbout from "./About/HomeAbout";
+import HomeGallery from "./Gallery/HomeGallery";
+
+import Banner from "./Home/banner";
+import HomeMenu from "./Menu/HomeMenu";
+import Question from "./Question";
+import SearchRecipe from "./Search/SearchRecipe";
 const Home = () => {
-	const Menu = useRecoilValue(MenuBtn);
-
 	return (
-		<div className="banner">
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-4">
-						<Header />
-						<MenuSide />
-						<div className="banner-area">
-							<h6>WIDE OPTIONS OF CHOICE</h6>
-							<h1>Delicious Recipes</h1>
-							<p>
-								inappropriate behavior is often laughed off as “boys will be
-								boys,” women face higher conduct standards especially in the
-								workplace. That’s why it’s crucial that, as women.
-							</p>
-
-							<button>Check our Menu</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<>
+			<Banner />
+			<HomeAbout />
+			<HomeMenu />
+			<Question />
+			<HomeGallery />
+			<SearchRecipe />
+		</>
 	);
 };
 
