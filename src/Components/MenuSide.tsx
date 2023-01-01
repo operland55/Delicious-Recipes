fa2b0d2da9bbcac2e5094529005a2ec726f9bb78
jungpa <operland55@naver.com>
2023-01-01 11:39:02 +0900

    ✨ Make Side Menu

--- src/Components/MenuSide.tsx
+++ src/Components/MenuSide.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
 import { MenuBtn } from "../atom";
 
 const MenuSide = () => {
 	const [Menu, setMenu] = useRecoilState(MenuBtn);
+	const navigate = useNavigate();
 	const list = ["Home", "about", "menu", "gallery", "blog", "search", "concat"];
 	return (
 		<nav className="sm-only">
 			<ul className={Menu ? "menuItem active sm-only" : "menuItem sm-only"}>
 				{list.map((item, key) => (
-					<li key={key}>{item}</li>
+					<li
+						key={key}
+						onClick={() => {
+							navigate(`/${item}`);
+						}}
+					>
+						{item}
+					</li>
 				))}
 			</ul>
 
