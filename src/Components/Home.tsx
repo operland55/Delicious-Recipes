b7a3f4e0d8c1bdfdd9ace6ffc50e4381c9219e90
jungpa <operland55@naver.com>
2023-01-01 14:41:08 +0900

    🚧 .

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import HomeAbout from "./About/HomeAbout";
 import HomeGallery from "./Gallery/HomeGallery";
 
-import Banner from "./Home/banner";
+import Banner from "./home/banner";
 import HomeMenu from "./Menu/HomeMenu";
 import Question from "./Question";
 import SearchRecipe from "./Search/SearchRecipe";
