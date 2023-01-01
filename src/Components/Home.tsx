c0d66f602263fa9856fe925b999df5877f85ca81
jungpa <operland55@naver.com>
2023-01-01 14:34:09 +0900

    🚀

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import HomeAbout from "./About/HomeAbout";
 import HomeGallery from "./Gallery/HomeGallery";
 
-import Banner from "./Home/banner";
+// import Banner from "./Home/banner";
 import HomeMenu from "./Menu/HomeMenu";
 import Question from "./Question";
 import SearchRecipe from "./Search/SearchRecipe";
 const Home = () => {
 	return (
 		<>
-			<Banner />
+			{/* <Banner /> */}
 			<HomeAbout />
 			<HomeMenu />
 			<Question />
