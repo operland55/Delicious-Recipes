e29ef3d3b6c57153d10264a0f2a686dc72628cc6
jungpa <operland55@naver.com>
2023-01-02 19:47:00 +0900

    ✨ Make AboutBanner Component and style

--- src/Components/Home/AboutBanner.tsx
+++ src/Components/Home/AboutBanner.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const AboutBanner = () => {
 	const location = useLocation();
 	console.log(location);
+
+	const name = location.pathname.replace("/", "");
 	return (
 		<div className="aboutbanner">
 			<div className="container">
 				<div className="row">
 					<div className="col-sm-4">
-						<h1>Home &gt {location.pathname}</h1>
+						<div className="title">
+							<h1>{name}</h1>
+							<p>
+								Home <AiOutlineArrowRight />
+								{name}{" "}
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
