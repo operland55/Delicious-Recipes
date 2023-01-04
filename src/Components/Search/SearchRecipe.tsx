8120657d869dbb3cdd5b0e413b9020434bb21500
jungpa <operland55@naver.com>
2023-01-04 17:23:22 +0900

    ⚰️ React-query Delete

--- src/Components/Search/SearchRecipe.tsx
+++ src/Components/Search/SearchRecipe.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { BsSearch } from "react-icons/bs";
-import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { searchApi } from "../../api";
 import SearchList from "./SearchList";
 const SearchRecipe = () => {
 	const [input, setInput] = useState("");
-	const { data: searchData, isLoading } = useQuery("search", () => {
-		return searchApi(input);
-	});
+
 	const navigate = useNavigate();
-	let values = searchData?.results;
 	const submitHandler = (e) => {
 		e.preventDefault();
 		navigate(`/search/${input}`);
