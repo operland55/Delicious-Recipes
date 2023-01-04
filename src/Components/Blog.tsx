aa797b53263f91583f5b0e1e6dab11470729bdf0
jungpa <operland55@naver.com>
2023-01-04 17:20:47 +0900

    ✨ Make Blog Component

---
+++ src/Components/Blog.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AboutBanner from "./Home/AboutBanner";
+import MenuSide from "./MenuSide";
+
+const Blog = () => {
+	return (
+		<>
+			<AboutBanner />
+
+			<MenuSide />
+		</>
+	);
+};
+
+export default Blog;
