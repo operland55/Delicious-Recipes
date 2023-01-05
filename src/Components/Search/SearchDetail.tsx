25147deb27bede893be5790fe7f021db7e58f9b6
jungpa <operland55@naver.com>
2023-01-05 17:08:44 +0900

    ✨ Details after clicking

--- src/Components/Search/SearchDetail.tsx
+++ src/Components/Search/SearchDetail.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AboutBanner from "../Home/AboutBanner";
 import MenuSide from "../MenuSide";
-
+import { AiOutlineClose } from "react-icons/ai";
+import { useSetRecoilState } from "recoil";
+import { DetailBtn } from "../../atom";
 function SearchDetail() {
 	const [details, setDetails] = useState<any>();
-	// const [activeTab, setActiveTab] = useState("instructions");
-	const api = "7d141da6f44b471d9643dc861ff6cc00";
+	const setBtn = useSetRecoilState(DetailBtn);
+	const api = "d9ed831d48d94f28b8f9aa33d2de1a8a";
 	const detail = useParams();
 	const fetchDetails = async () => {
 		const data = await fetch(
@@ -19,17 +21,39 @@ function SearchDetail() {
 	useEffect(() => {
 		fetchDetails();
 	}, [detail.id]);
-	console.log("ssss", details);
+	console.log("ssss", details?.extendedIngredients);
 	return (
 		<>
-			<AboutBanner />
-
 			<div className="detail">
 				<div className="container">
 					<div className="row">
 						<div className="col-sm-4">
-							<div>
-								<h2>{details?.title}</h2>
+							<div className="detail-info">
+								<AiOutlineClose
+									onClick={() => {
+										setBtn(false);
+									}}
+								/>
+								<div className="textImgBox">
+									<p>{details?.title}</p>
+									<div className="img-box">
+										<img src={details?.image} alt={details?.title}></img>
+									</div>
+								</div>
+								<ul className="recipe-lists">
+									<li>
+										<h1>Ingredients</h1>
+									</li>
+									{details?.extendedIngredients.map((ingredient, index) => {
+										return (
+											<>
+												<li key={ingredient.id}>
+													{index + 1}: {ingredient.original}
+												</li>
+											</>
+										);
+									})}
+								</ul>
 							</div>
 						</div>
 					</div>
